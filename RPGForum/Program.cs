5b7ae4a724d8f8b480a1e91b8f8a9d95148bc36e
João Abreu <jaabrel@outlook.pt>
2026-03-18 11:47:15 +0000

    Adição do Swagger

--- RPGForum/Program.cs
+++ RPGForum/Program.cs
@@ -14,12 +14,17 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
