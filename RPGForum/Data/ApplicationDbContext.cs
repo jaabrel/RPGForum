539ea7eb29f7eac4e5182c5ad59a05a6d5c07b58
João Abreu <jaabrel@outlook.pt>
2026-03-18 09:49:45 +0000

    Adicionar arquivos de projeto.

---
+++ RPGForum/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RPGForum.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
