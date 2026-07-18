4ee319ba5752019821c3fa1b59d1ce69edbf2886
Rodrigo Pinheiro <aluno26402@ipt.pt>
2026-07-18 22:23:03 +0100

    Criada pagina Sobre separada e removido conteudo do index de acordo com a solicitacao

---
+++ RPGForum/Pages/Sobre.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RPGForum.Pages
+{
+    public class SobreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
